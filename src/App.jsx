7d392d5a9002948f7e8b2ffc2d046a6b56a4002d
ayyappan97899 <ayyappan97899@gmail.com>
2024-06-28 15:59:18 +0530

    husky precommit changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
-  const hjj = '';
 
   return (
     <>
